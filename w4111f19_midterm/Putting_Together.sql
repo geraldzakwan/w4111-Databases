0e8ba7135b393e08afc62cb42d8b14101620c60e
geraldzakwan <geraldi.dzakwan@gmail.com>
2019-10-24 01:21:51 -0400

    Pake Appearances

--- w4111f19_midterm/Putting_Together.sql
+++ w4111f19_midterm/Putting_Together.sql
@@ -14,17 +14,34 @@ use lahman2019clean;
 DROP TABLE IF EXISTS salaries_clean;
 
 CREATE TABLE `salaries_clean` (
-  `yearID` varchar(4),
-  `teamID` varchar(3),
-  `lgID` varchar(2),
-  `playerID` varchar(16),
-  `salary` unsigned,
-  PRIMARY KEY (playerID, teamID, yearID, lgID),
-  FOREIGN KEY (playerID, teamID, yearID) REFERENCES Appearances(playerID, teamID, yearID)
+   `yearID` varchar(4),
+   `teamID` varchar(4),
+   `lgID` varchar(2),
+   `playerID` varchar(16),
+   `salary` int unsigned,
+   PRIMARY KEY (playerID, teamID, yearID, lgID),
+   -- FOREIGN KEY (playerID, teamID, yearID) REFERENCES Appearances(playerID, teamID, yearID)
+   FOREIGN KEY (playerID) REFERENCES People(playerID),
+   FOREIGN KEY (teamID, yearID, lgID) REFERENCES Teams(teamID, yearID, lgID),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO salaries_clean
+SELECT * FROM salaries;
+
+/* 
+SELECT playerID, teamID, yearID, salary FROM salaries WHERE NOT EXISTS (
+	SELECT playerID, teamID, yearID FROM appearances
+	WHERE salaries.playerID = appearances.playerID AND salaries.teamID = appearances.teamID AND salaries.yearID = appearances.yearID
+);
+*/
 
 -- SELECT teamID, yearID FROM Teams LIMIT 50;
 
 -- DESC appearances;
 
+-- SELECT playerID FROM appearances WHERE G_all IS NULL;
+
+-- SELECT COUNT(*) FROM appearances;
+
 -- SELECT DISTINCT LENGTH(playerID) FROM People;
+
