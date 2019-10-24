c41940adb3fe6b0f4ed07deefd6bc9279e56d4e3
geraldzakwan <geraldi.dzakwan@gmail.com>
2019-10-23 21:40:07 -0400

    Finish ER diagram

---
+++ w4111f19_midterm/country_codes.sql
@@ -0,0 +1,12 @@
+USE classicmodels;
+-- SELECT country, country_code FROM customers_clean WHERE country_code IS NULL;
+
+-- SELECT name FROM countrycodes;
+
+-- SET SQL_SAFE_UPDATES = 0;
+-- UPDATE customers_clean SET country = 'United States' WHERE country = 'USA';
+-- UPDATE customers_clean SET country = 'United Kingdom' WHERE country = 'UK';
+-- UPDATE customers_clean SET country = 'United States' WHERE country = 'US';
+-- SET SQL_SAFE_UPDATES = 1;
+
+-- DESC customers_clean;
